81f1e86c0197415154f66bd6eefc8fe3014d6066
Luan Martins <luanm.lima366@gmail.com>
2023-07-08 10:03:15 -0300

    Criação de classe responsavel por mostrar as informações de pensamentos na tela

---
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.css']
+})
+export class PensamentoComponent {
+  @Input() pensamento = {
+    conteudo: '',
+    autoria: '',
+    modelo: ''
+  }
+
+  larguraPensamento(): string {
+    if(this.pensamento.conteudo.length >= 256){
+      return 'pensamento-g'
+    }
+    return 'pensamento-p'
+  }
+
+}
